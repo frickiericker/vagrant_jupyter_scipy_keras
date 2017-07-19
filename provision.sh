8f7d7520e5cf9f3a3fb1d726be5effb439ed010c
frickiericker <frickiericker@gmail.com>
2017-07-19 12:50:04 +0900

    Update provisioner

--- provision.sh
+++ provision.sh
@@ -12,6 +12,7 @@ doit apt-get update
 doit apt-get install -y python-dev python-pip
 doit apt-get install -y python-numpy python-scipy python-matplotlib python-sklearn python-pil
 doit apt-get install -y ipython ipython-notebook
+doit pip install --upgrade pip
 doit pip install tensorflow theano keras seaborn
 doit pip install jupyter jupyter_contrib_nbextensions
 doit jupyter contrib nbextension install 2> /dev/null
